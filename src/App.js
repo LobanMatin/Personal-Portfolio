0a327c519969c21ad861163aa2b36b1f816a7a7b
Loban Matin <loban.matin@gmail.com>
2024-01-13 17:57:11 +1030

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //Palette: https://colorhunt.co/palette/27005d9400ffaed2ffe4f1ff
 import './App.css';
+import Nav from './Nav'
 import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <div className="content">
+        <Nav />
         <Home />
       </div>
     </div>
