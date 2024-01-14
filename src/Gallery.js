b2840fbccd9db31dbc5622a4020dff377f8927c6
Loban Matin <loban.matin@gmail.com>
2024-01-14 16:33:34 +1030

    gallery created

---
+++ src/Gallery.js
@@ -0,0 +1,11 @@
+import './Gallery.css';
+
+const Gallery = () => {
+    return (
+        <div className="Gallery">
+            <h3>Gallery</h3>
+        </div>
+    );
+}
+ 
+export default Gallery;
