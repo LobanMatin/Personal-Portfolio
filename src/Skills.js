7d1adc5d42ac28e4ea62c96f6ad9ac5f920779bd
Loban Matin <loban.matin@gmail.com>
2024-01-14 17:09:41 +1030

    updated page

---
+++ src/Skills.js
@@ -0,0 +1,26 @@
+import './Skills.css';
+
+const Gallery = () => {
+    return (
+        <div className="Skills">
+            <h3>SKILLS</h3>
+            <div className="skills-content">
+                <div className="laptop">
+                </div>
+                <div className="skills-list">
+                    <div className="skill-section">
+                        <h2>Web Development</h2>
+                    </div>
+                    <div className="skill-section">
+                        <h2>Programming</h2>
+                    </div>
+                    <div className="skill-section">
+                        <h2>Engineering</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default Gallery;
