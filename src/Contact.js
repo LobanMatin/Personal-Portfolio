d11e0ca6c736cf36f77bff5dc912b77bdeb8588d
Loban Matin <loban.matin@gmail.com>
2024-01-15 14:53:22 +1030

    updated

--- src/Contact.js
+++ src/Contact.js
@@ -3,7 +3,18 @@ import './Contact.css';
 const Contact = () => {
     return (
         <div className="Contact">
-            <h3>Get in Touch!</h3>
+            <div className="contact-content">
+                <h3>Get in Touch:</h3>
+                
+            </div>
+            <div className="footer">
+                <p style={{fontWeight:"600", fontSize:"1.2em"}}>Loban Matin</p>
+                <p>Master of Mechatronics Engineering Student</p>
+                <br />
+                <p>University of Melbourne</p>
+                <p>Melbourne, Australia</p>
+                
+            </div>
         </div>
     );
 }
